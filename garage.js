4e53a1f206c21f4bea7d70ea0e3f280555eed0c7
gerry <weirich@elexis.ch>
2017-03-21 17:46:13 +0100

    details

--- garage.js
+++ garage.js
@@ -1,14 +1,15 @@
 // garage.js
 // http://blog.mgechev.com/2014/02/19/create-https-tls-ssl-application-with-express-nodejs/
-var pin=38
-var realpi
+
+var pin=38            // Pin (not GPIO #) to handle
+var realpi=false      // Set to true if running really on the Raspberry Pi (otherwise GPIO access is simulated)
 var fs=require('fs')
 var https=require('https')
 var express=require('express')
 var nconf=require('nconf')
 var hash=require('crypto-js/sha256')
 var path=require('path')
-var salt="um hackern mit rainbow tables die Suppe zu versalzen"
+var salt="um Hackern mit 'rainbow tables' die Suppe zu versalzen"
 nconf.env().argv().file('users.json')
 var app=express()
 
@@ -40,7 +41,6 @@ app.get("/garage",function(request,response){
   var password=JSON.stringify(hash(request.query.password+salt))
   var valid=nconf.get(user)
   if(valid && valid == password){
-    console.log("Das Garagentor tut etwas!")
     rpio.write(pin,rpio.HIGH)
     rpio.sleep(1)
     rpio.write(pin,rpio.LOW)
@@ -50,22 +50,6 @@ app.get("/garage",function(request,response){
   }
 })
 
-/*
-app.get("/garage/:user/:password",function(request,response){
-  var user=JSON.stringify(hash(request.params['user']+salt))
-  var password=JSON.stringify(hash(request.params['password']+salt))
-  var valid=nconf.get(user)
-  if(valid && valid == password){
-    console.log("Das Garagentor tut etwas!")
-    rpio.write(pin,rpio.HIGH)
-    rpio.sleep(1)
-    rpio.write(pin,rpio.LOW)
-    response.send("Auftrag ausgeführt, "+request.params['user'])
-  }else{
-    response.send("Wer bist denn du???")
-  }
-})
-*/
 app.get("/adduser/:username/:password",function(req,resp){
   var user=JSON.stringify(hash(req.params['username']+salt))
   var password=JSON.stringify(hash(req.params['password']+salt))
@@ -74,8 +58,3 @@ app.get("/adduser/:username/:password",function(req,resp){
   resp.send("Ok")
 })
 
-/*
-app.listen(3000,function(){
-  console.log("Garagenserver läuft an port 3000")
-})
-*/
