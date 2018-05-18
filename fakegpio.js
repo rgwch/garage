096d6e381addb1cf8fdee36e2b7b467b5b293273
gerry <weirich@elexis.ch>
2018-05-18 17:56:19 +0200

    det

--- fakegpio.js
+++ fakegpio.js
@@ -1,3 +1,9 @@
+/**
+ *  Garagentor-Fernbedienung mit Raspberry Pi
+ *  (c) 2017-2018 by G. Weirich
+ * 
+ * Mock als Ersatz für die RaspberryGPIOS
+ **/
 module.exports=class Gpio{
     constructor(pin,dir){}
     writeSync(){}
